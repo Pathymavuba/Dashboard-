18544fe5164d046c3aef311f4e5aae5dc3946f73
pathy <mavubapathy@gmail.com>
2022-06-28 12:15:49 +0100

    work finished

---
+++ orders.js
@@ -0,0 +1,37 @@
+const Orders = [
+    {
+        productName:'Foldable mini Drone',
+        productNumber:'85631',
+        paymentSatus:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName:'LARVENDER KF102 Drone',
+        productNumber:'85631',
+        paymentSatus:'Due',
+        shipping:'Declined'
+
+    },
+    {
+        productName:'Foldable mini Drone',
+        productNumber:'85631',
+        paymentSatus:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName:'Foldable mini Drone',
+        productNumber:'85631',
+        paymentSatus:'Due',
+        shipping:'Declined'
+
+    },
+    {
+        productName:'Foldable mini Drone',
+        productNumber:'85631',
+        paymentSatus:'Due',
+        shipping:'Pending'
+
+    },
+]
